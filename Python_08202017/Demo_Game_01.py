cded45bb948f3ed75ea4d34192a2012386728253
Jay Wen <wwasteland@163.com>
2017-08-20 13:55:11 +0800

    Python add 08202017 by Jay Wen.

---
+++ Python_08202017/Demo_Game_01.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+print("GAME START!");
+tempValue = input("Please input your number.");
+guess = int(tempValue);
+
+if  guess == 7:
+    print('You are right.');
+else:
+    print('You are error.');
+#转义字符串 \'
+print("GAME\'s OVER!");
---
+++ Python_08202017/HelloWord.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+#__author__ = "Jay Wen"
+
+print("Hello Word");
+print("Hello Word");
+print("Hello Word");
+print("Hello Word");
+print("Hello Word");
+print("Hello Word");
